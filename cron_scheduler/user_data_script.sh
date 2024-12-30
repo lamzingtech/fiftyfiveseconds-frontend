04e1671577e5d85790d8efcb84f26c1592290650
Yaiphaba-Ng <yaiphabalamzing@gmail.com>
2024-12-30 09:33:43 +0530

    changed user data script

--- cron_scheduler/user_data_script.sh
+++ cron_scheduler/user_data_script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo docker start 8c277d87242a
-sudo docker exec 8c277d87242a /bin/bash -c "cd /root/55-seconds && conda run -n openvoice ./start.sh"
+# sudo docker exec 8c277d87242a /bin/bash -c "cd /root/55-seconds && conda run -n openvoice ./start.sh"
+sudo docker exec 8c277d87242a /bin/bash -c "cd /root/55-seconds && conda run -n openvoice uvicorn app:app --host 0.0.0.0 --port 8000"
 
 # cd /root/55-seconds && ./script.sh
