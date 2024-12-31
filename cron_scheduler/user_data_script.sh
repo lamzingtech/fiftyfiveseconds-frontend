d508993cc1a2b3198fa8510ab7605e195f20a234
Yaiphaba-Ng <yaiphabalamzing@gmail.com>
2024-12-31 14:37:48 +0530

    empty user_data_script

--- cron_scheduler/user_data_script.sh
+++ cron_scheduler/user_data_script.sh
@@ -1,3 +1,1 @@
 #!/bin/bash
-sudo docker start 0287923e5f6c
-sudo docker exec 0287923e5f6c /bin/bash -c "kill -9 \`lsof -t -i:8000\` && cd /root/55-seconds && uvicorn app:app --host 0.0.0.0 --port 8000"
